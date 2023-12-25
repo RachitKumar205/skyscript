716dde78c3234e6105b0a6c2dd94842ea05ffd92
rachitkumar205 <rk_programmer@outlook.com>
2023-12-25 23:41:05 +0530

    Added readme

---
+++ Readme.md
@@ -0,0 +1,45 @@
+# SkyScript
+
+
+SkyScript is a weather status checker powered by NextJS and nextUI. SkyScript gets your location and tells you about the current weather updates in your locality, with no clutter. 
+
+## Features
+
+- Check the weather
+- Nothing else, we believe in avoiding distractions
+
+## Tech
+
+Dillinger uses a number of open source projects to work properly:
+
+- NextJS
+- NextUI Component Library
+- OpenWeatherMap
+
+## Installation
+
+
+Create a .env.local file, and store your OpenWeaterMap API Key in it
+
+Install the dependencies.
+
+```sh
+npm install
+```
+
+Run in development:
+
+```sh
+npm run dev
+```
+
+Open http://localhost:3000 in your browser to access the page
+
+
+
+## License
+
+MIT
+
+
+
--- app/page.js
+++ app/page.js
@@ -1,113 +1,162 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Switch
+} from "@nextui-org/react";
+import Link from "next/link";
+
+const Page = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [temperatureUnit, setTemperatureUnit] = useState('K');
+  const [currentTemperature, setCurrentTemperature] = useState(null);
+  const [icon, setIcon] = useState(null);
+  const [bgClass, setBgClass] = useState('clear-bg');
+
+
+  useEffect(() => {
+    // Get user location using Geolocation API
+    const getUserLocation = async () => {
+      if (navigator.geolocation) {
+        try {
+          await navigator.geolocation.getCurrentPosition(
+              (position) => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+              },
+              (error) => {
+                console.error("Error getting location:", error);
+                setError("Unable to determine your location. Please try again or enter it manually.");
+              }
+          );
+        } catch (error) {
+          console.error("Error accessing Geolocation API:", error);
+          setError("Geolocation is not supported in your browser.");
+        }
+      } else {
+        console.error("Geolocation API is not supported in this browser.");
+        setError("Geolocation is not supported in your browser.");
+      }
+    };
+
+    getUserLocation();
+  }, []);
+
+  useEffect(() => {
+    // Fetch weather data only after latitude and longitude are available
+    if (latitude && longitude) {
+      const fetchWeather = async () => {
+        try {
+          const apiKey = process.env.NEXT_PUBLIC_API_KEY; // Replace with your actual API key
+          const response = await axios.get(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`,
+              {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              }
+          );
+          setWeatherData(response.data);
+          setCurrentTemperature(response.data.main.temp);
+          const src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+          setIcon(src);
+          console.log(icon);
+        } catch (error) {
+          setError(error);
+        }
+      };
+
+      fetchWeather();
+    }
+  }, [latitude, longitude]);
+
+  const handleUnitChange = () => {
+    setTemperatureUnit(temperatureUnit === 'K' ? 'C' : 'K');
+    setCurrentTemperature(
+        temperatureUnit === 'K'
+            ? weatherData.main.temp - 273.15 // Convert to Celsius
+            : weatherData.main.temp + 273.15 // Convert to Kelvin
+    );
+  };
+
+  useEffect(() => {
+    if (weatherData) {
+      const mainWeather = weatherData.weather[0].main;
+      setBgClass(
+          mainWeather === 'Clear'
+              ? 'min-h-screen w-full clear-bg'
+              : mainWeather === 'Thunderstorm' || mainWeather === 'Clouds'
+                  ? 'min-h-screen w-full thunderstorm-bg'
+                  : 'min-h-screen w-full clouds-bg'
+      );
+    }
+  }, [weatherData]);
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={bgClass}>
+        <div className="flex items-center justify-end md:w-1/2 h-screen">
+          <div className="w-full md:w-2/3">
+            {error ? (
+                <p>Error fetching weather data: {error.message}</p>
+            ) : weatherData ? (
+                <Table aria-label="Example empty table" className="w-full h-full">
+                  <TableHeader>
+                    <TableColumn>
+                      <p className="text-2xl md:text-5xl m-2">{weatherData.name}</p>
+                    </TableColumn>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        <p className="text-lg md:text-4xl">
+                          {currentTemperature.toFixed(1)}
+                          {temperatureUnit}
+                        </p>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <div className="flex flex-col gap-2">
+                          <Switch onValueChange={handleUnitChange}></Switch>
+                          <p className="text-xs md:text-small text-default-500">
+                            {temperatureUnit === 'K'
+                                ? 'change to celsius'
+                                : 'change to kelvin'}
+                          </p>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <p className="text-xs md:text-2xl">
+                          {weatherData.weather[0].description}
+                        </p>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+            ) : (
+                <p>Loading weather data...</p>
+            )}
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
